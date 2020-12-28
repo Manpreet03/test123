d32a9b81ed3d2c8d78913914e632304f653002f0
Manpreet Hire <manpreet0307@outlook.com>
2020-12-28 07:04:35 +0530

    hello

--- sketch.js
+++ sketch.js
@@ -33,7 +33,8 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    box6 = new Box(10,10,10,10);
+
 
 }
 
@@ -58,5 +59,6 @@ function draw(){
     log4.display();
     log5.display();
 
-    bird.display();
+    box6.display();
+
 }
